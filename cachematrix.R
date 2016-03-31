0e5888e5f8fab6627ce60ecf564f6fb7b2b4d4f8
ShuaiGitHub <ShuaiGitHub@users.noreply.github.com>
2016-03-31 13:46:24 -0700

    Update cachematrix.R
    remove the test code attached at below

--- cachematrix.R
+++ cachematrix.R
@@ -41,10 +41,3 @@ cacheSolve <- function(x, ...) {
         return(cache)
         }
 }
-N <- 3
-M <- N
-set.seed(1)
-test <- matrix(rnorm(N*M,mean=0,sd=1), N, M)
-matrix_vector <- makeCacheMatrix(test)
-cache_solve <-cacheSolve(matrix_vector)
-cache_solve <-cacheSolve(matrix_vector)
