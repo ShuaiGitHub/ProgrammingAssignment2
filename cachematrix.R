d2227d59decf8105f0198e2021a8fc2c3a2ba1ec
Shuai <sudapink@gmail.com>
2016-03-31 11:40:04 -0700

    First change
    Follow the description of make Vector to write make Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## This assignment is similar the make vector function we have seen in the example.
 ## Write a short comment describing this function
+## the makeCacheMatrix will return a list contains three functions and one variable:
+## set: update the current matrix to a new matrix
+## get: return the current matrix
+## setInv: set the inverse to the variable cache
+## getInv: get the inverse of the current cache
 
 makeCacheMatrix <- function(x = matrix()) {
+    cache <- NULL
+    set <-function(y){
+        x <<- y
+        cache <- NULL
+    }
+    get <- function () x
+    setInver
 
 }
 
